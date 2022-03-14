3c89e83bf39743bf150d8ea8c5c5f69e5ab542f1
Siddhant <siddhantsrivastava0811@gmail.com>
2022-03-14 20:47:07 +0530

    addLetter & deleteLetter function are created

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const keyboard = document.querySelector('.keyboard');
 const tile = document.querySelector('.tile');
 
+let row=0;
+let col=0;
 
+const wordle = 'BUILDING'
 const keys = [
     'Q',
     'W',
@@ -42,7 +45,7 @@ const guessRows = [
     ['', '', '', '', '']
 ]
 
-
+//Tiles
 guessRows.forEach((guessRow, guessRowIndex) => {
        const rowElement =  document.createElement('div');
         rowElement.setAttribute('id', 'guessRow-'+guessRowIndex);
@@ -59,17 +62,49 @@ guessRows.forEach((guessRow, guessRowIndex) => {
 
 
 
-
-
-const handleClick = () => {
-    console.log('clicked');
-}
-
+//keyboard
 keys.forEach(key => {
     const buttonElement = document.createElement('button');
     buttonElement.textContent=key;
     buttonElement.setAttribute('id', key);
-    buttonElement.addEventListener('click', handleClick);
+    buttonElement.addEventListener('click',()=> handleClick(key));
 
     keyboard.append(buttonElement);
 })
+
+const handleClick = (key) => {
+    console.log('clicked', key);
+    if(key==='«'){
+        deleteLetter();
+        console.log(guessRows);
+        return
+    }
+    if(key==='ENTER'){
+        console.log(guessRows);
+        return
+    }
+    addLetter(key);
+    console.log(guessRows);
+}
+
+
+const addLetter = (letter)=>{
+    if(row<5&&col<6){
+const cell=  document.getElementById('guessRow-'+ row +'-tile-'+ col);
+cell.textContent = letter;
+guessRows[row][col]=letter;
+cell.setAttribute('data', letter);
+col++;
+console.log(guessRows);
+    }
+}
+
+const deleteLetter = ()=>{
+    if(col>0){
+    col--;
+    const cell=  document.getElementById('guessRow-'+ row +'-tile-'+ col);
+    cell.textContent='';
+    guessRows[row][col]='';
+cell.setAttribute('data', '');
+    }
+}
