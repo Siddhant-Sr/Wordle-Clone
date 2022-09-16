4e6f4b094ccc0c7c4d39a1771689d5980d49a164
Siddhant Srivastava <67307537+Siddhant-Sr@users.noreply.github.com>
2022-09-16 12:34:23 +0530

    Project Completed

--- app.js
+++ app.js
@@ -1,250 +1,188 @@
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.key-container')
+const messageDisplay = document.querySelector('.message-container')
 
+let wordle = "SUPER"
 
-
-const keyboard = document.querySelector(".keyboard");
-const tile = document.querySelector(".tile");
-const message = document.querySelector(".message");
-
-let row = 0;
-let col = 0;
-let isGameOver = false;
-
-let wordle="SUPER"
-
-// const getWordle=()=>{
-//   fetch('http://localhost:8000/word').then(response=> response.json()).then(json => {
-//     console.log(json)
-//     wordle = json.toUpperCase()
-//   }).catch(err => console.log(err))
+// const getWordle = () => {
+//     fetch('http://localhost:8000/word')
+//         .then(response => response.json())
+//         .then(json => {
+//             wordle = json.toUpperCase()
+//         })
+//         .catch(err => console.log(err))
 // }
-// getWordle();
+// getWordle()
+
 const keys = [
-  "Q",
-  "W",
-  "E",
-  "R",
-  "T",
-  "Y",
-  "U",
-  "I",
-  "O",
-  "P",
-  "A",
-  "S",
-  "D",
-  "F",
-  "G",
-  "H",
-  "J",
-  "K",
-  "L",
-  "ENTER",
-  "Z",
-  "X",
-  "C",
-  "V",
-  "B",
-  "N",
-  "M",
-  "«",
-];
-
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+]
 const guessRows = [
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-  ["", "", "", "", ""],
-];
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', '']
+]
+let currentRow = 0
+let currentTile = 0
+let isGameOver = false
 
-//Tiles
 guessRows.forEach((guessRow, guessRowIndex) => {
-  const rowElement = document.createElement("div");
-  rowElement.setAttribute("id", "guessRow-" + guessRowIndex);
-  guessRow.forEach((guess, guessIndex) => {
-    const tileElement = document.createElement("div");
-    tileElement.setAttribute(
-      "id",
-      "guessRow-" + guessRowIndex + "-tile-" + guessIndex
-    );
-    tileElement.classList.add("tiles");
-    rowElement.append(tileElement);
-  });
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+    guessRow.forEach((_guess, guessIndex) => {
+        const tileElement = document.createElement('div')
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+        tileElement.classList.add('tile')
+        rowElement.append(tileElement)
+    })
+    tileDisplay.append(rowElement)
+})
 
-  tile.append(rowElement);
-});
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', () => handleClick(key))
+    keyboard.append(buttonElement)
+})
 
-//keyboard
-keys.forEach((key) => {
-  const buttonElement = document.createElement("button");
-  buttonElement.textContent = key;
-  buttonElement.setAttribute("id", key);
-  buttonElement.addEventListener("click", () => handleClick(key));
-
-  keyboard.append(buttonElement);
-});
-
-const handleClick = (key) => {
-  if(!isGameOver){
-  console.log("clicked", key);
-  if (key === "«") {
-    deleteLetter();
-    console.log(guessRows);
-    return;
-  }
-  if (key === "ENTER") {
-    checkRow();
-    console.log(guessRows);
-    return;
-  }
-  addLetter(key);
-  console.log(guessRows);
-}
+const handleClick = (letter) => {
+    if (!isGameOver) {
+        if (letter === '«') {
+            deleteLetter()
+            return
+        }
+        if (letter === 'ENTER') {
+            checkRow()
+            return
+        }
+        addLetter(letter)
+    }
 }
 
 const addLetter = (letter) => {
-  if (row < 5 && col < 6) {
-    const cell = document.getElementById("guessRow-" + row + "-tile-" + col);
-    cell.textContent = letter;
-    guessRows[row][col] = letter;
-    cell.setAttribute("data", letter);
-    col++;
-    console.log(guessRows);
-  }
-};
+    if (currentTile < 5 && currentRow < 6) {
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+        tile.textContent = letter
+        guessRows[currentRow][currentTile] = letter
+        tile.setAttribute('data', letter)
+        currentTile++
+    }
+}
 
 const deleteLetter = () => {
-  if (col > 0) {
-    col--;
-    const cell = document.getElementById("guessRow-" + row + "-tile-" + col);
-    cell.textContent = "";
-    guessRows[row][col] = "";
-    cell.setAttribute("data", "");
-  }
-};
-
-//  currentTile -> col and cirrentRow -> row
-// Dictionary implementation
-// const checkRow = () =>{
-//   const guess = guessRows[row].join('');
-//   if(col>4){
-//     // DICTIONSRY API
-//     fetch('http://localhost:8000/check/?word=${guess}').then(response=>response.json()).then(json=> {
-//       console.log(json)
-//       if(json == 'Entry word not found'){
-//         showMessage('Word does not exist')
-//         return
-//       }
-//       else{
-//         console.log(guess, wordle);
-//         flipTile();
-//         if(wordle==guess){
-//        showMessage('Magnificent!');
-//           isGameOver = true
-//           return
-//       }else{
-//       if(row >=5){
-//         showMessage('Game Over');
-//         isGameOver=true
-//         return
-//       }
-//       if(row<5){
-//         row++;
-//         col = 0;
-//         return;
-//       }
-//     }
-//       }
-//     })
-
-//   }
-// }
-
-
-const checkRow = () =>{
-  const guess = guessRows[row].join('');
-  if(col>4){
- getData(guess)
-console.log('i am atafter getdata');
-    flipTile();
-    if(wordle==guess){
-   showMessage('Magnificent!');
-      isGameOver = true
-      return
-  }else{
-  if(row >=5){
-    showMessage('Game Over');
-    isGameOver=true
-    return
-  }
-  if(row<5){
-    row++;
-    col = 0;
-    return;
-  }
-}
-  }
+    if (currentTile > 0) {
+        currentTile--
+        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+        tile.textContent = ''
+        guessRows[currentRow][currentTile] = ''
+        tile.setAttribute('data', '')
+    }
 }
 
-async function getData(word){
-  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-  const data = await response.json()
-
-  const title =data.title
-  if(title == "No Definitions Found"){
-    showMessage('Word does not exist');
-    return
-  }
- 
-
+const checkRow = () => {
+    const guess = guessRows[currentRow].join('')
+    if (currentTile > 4) {
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${guess}`)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json.title)
+                if (json.title == 'No Definitions Found') {
+                    showMessage('word not in list')
+                    return
+                } else {
+                    flipTile()
+                    if (wordle == guess) {
+                        showMessage('Magnificent!')
+                        isGameOver = true
+                        return
+                    } else {
+                        if (currentRow >= 5) {
+                            isGameOver = true
+                            showMessage('Game Over')
+                            return
+                        }
+                        if (currentRow < 5) {
+                            currentRow++
+                            currentTile = 0
+                        }
+                    }
+                }     
+            }).catch(err => console.log(err))
+    }
 }
 
-const showMessage = (mes) =>{
-  const messageElement = document.createElement('p');
-  messageElement.textContent=mes;
-  message.append(messageElement)
-  setTimeout(() => {
-    
-    message.remove(messageElement)
-  }, 2000);
+const showMessage = (message) => {
+    const messageElement = document.createElement('p')
+    messageElement.textContent = message
+    messageDisplay.append(messageElement)
+    setTimeout(() => messageDisplay.removeChild(messageElement), 2000)
 }
 
 const addColorToKey = (keyLetter, color) => {
-  const key = document.getElementById(keyLetter)
-  key.classList.add(color)
+    const key = document.getElementById(keyLetter)
+    key.classList.add(color)
 }
 
-const flipTile = ()=>{
-  const rowTiles = document.querySelector('#guessRow-'+ row).childNodes
- let checkWordle = wordle
- const guess = []
+const flipTile = () => {
+    const rowTiles = document.querySelector('#guessRow-' + currentRow).childNodes
+    let checkWordle = wordle
+    const guess = []
 
-rowTiles.forEach(tile => {
-  guess.push({letter: tile.getAttribute('data'), color: 'gray-overlay'})
+    rowTiles.forEach(tile => {
+        guess.push({letter: tile.getAttribute('data'), color: 'grey-overlay'})
+    })
 
-})
+    guess.forEach((guess, index) => {
+        if (guess.letter == wordle[index]) {
+            guess.color = 'green-overlay'
+            checkWordle = checkWordle.replace(guess.letter, '')
+        }
+    })
 
-guess.forEach((guess,index)=> {
-    if(guess.letter==wordle[index]){
-      guess.color = 'green-overlay'
-      checkWordle = checkWordle.replace(guess.letter, '')
-    }
-})
+    guess.forEach(guess => {
+        if (checkWordle.includes(guess.letter)) {
+            guess.color = 'yellow-overlay'
+            checkWordle = checkWordle.replace(guess.letter, '')
+        }
+    })
 
-guess.forEach((guess)=> {
-  if(checkWordle.includes(guess.letter)){
-    guess.color = 'yellow-overlay'
-    checkWordle = checkWordle.replace(guess.letter, '')
-  }
-})
-
-  rowTiles.forEach((tile, index) => {
-   
-   setTimeout(() => {
-    tile.classList.add('flip')
-    tile.classList.add(guess[index].color);
-    addColorToKey(guess[index].letter, guess[index].color)
-   }, 500 * index)
-  })
+    rowTiles.forEach((tile, index) => {
+        setTimeout(() => {
+            tile.classList.add('flip')
+            tile.classList.add(guess[index].color)
+            addColorToKey(guess[index].letter, guess[index].color)
+        }, 500 * index)
+    })
 }
+
--- index.js
+++ index.js
@@ -1,71 +1,31 @@
-const PORT =8000;
-const axios = require("axios");
-const express = require('express');
+const PORT = 8000
+const axios = require("axios").default
+const express = require("express")
+const cors = require("cors")
 require('dotenv').config()
-const cors = require('cors')
+const app = express()
 
+app.use(cors())
 
-const app =express().use(cors())
-
-app.get("/word", (req,res)=>{
- const options = {
+app.get('/word', (req, res) => {
+    const options = {
         method: 'GET',
         url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
         params: {count: '5', wordLength: '5'},
         headers: {
-          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-          'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+            'x-rapidapi-host': 'random-words5.p.rapidapi.com',
+            'x-rapidapi-key': process.env.RAPID_API_KEY
         }
-      };
-      
-      axios.request(options).then(function (response) {
-          console.log(response.data);
-          res.json(response.data[0]);
-      }).catch(function (error) {
-          console.error(error)
-      })
-    // const options = {
-    //     method: 'GET',
-    //     url: 'https://random-words5.p.rapidapi.com/getMultipleRandom',
-    //     params: {count: '5', wordLength: '5'},
-    //     headers: {
-    //       'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-    //       'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
-    //     }
-    //   };
-      
-    //   axios.request(options).then(function (response) {
-    //       console.log(response.data);
-    //       res.json(response.data[0]);
-    //   }).catch(function (error) {
-    //       console.error(error)
-    //   })
-})
-
-app.get('/check', (req, res)=>{
-    console.log(req);
-    
-   
-    
-    const options = {
-        method: 'GET',
-        url: 'https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary',
-        params: {word: 'bright'},
-        headers: {
-          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-          'X-RapidAPI-Host': 'dictionary-by-api-ninjas.p.rapidapi.com'
-        }
-      };
-      
-      axios.request(options).then(function (response) {
-        //   console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
-      
+    }
+    axios.request(options).then((response) => {
+        console.log(response.data)
+        res.json(response.data[0])
+    }).catch((error) => {
+        console.error(error)
+    })
 })
 
 
-app.listen(PORT, ()=> console.log('Server running on port '+ PORT))
 
-// https://dictionaryapi.com/api/v3/references/learners/json/test?key=44d86eb9-ff0b-499f-855f-d513b172da69
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT))
